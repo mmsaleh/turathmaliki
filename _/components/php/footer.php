12dadb9aa31e5ad2845245d539b3d71d340c2f88
Saleh <saleh.mayada@yahoo.com>
2014-02-07 20:54:29 +0300

    cloned app into turathmaliki

---
+++ _/components/php/footer.php
@@ -0,0 +1,12 @@
+<section class="container">
+	<footer class="row">
+		<nav class="col-lg-12">
+			<ul class="breadcrumb">
+				<li><a href="index.php">&copy; التراث المالكي  2014</a></li>
+				<li><a href="aboutproject.php">مشروع الكتاب الالكتروني</a></li>
+				<li><a href="contact.php">اتصل بنا</a></li>
+				
+			</ul><!-- breadcrumb -->
+		</nav><!-- nav -->
+	</footer><!-- footer -->
+</section><!-- container -->
---
+++ _/components/php/header.php
@@ -0,0 +1,32 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+			<a href="index.php"><img src="images/logo_maliki.png" alt="project logo"></a>	
+			</section><!-- branding -->
+			<section class="navbar navbar-default">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">الرئيسية</a></li>
+					<li><a href="about.php">حول الكتاب</a></li>
+					<li><a href="book.php">المتن</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">الشرح <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+							<li><a tabindex="-1" href="commentary.php">الحبل المتين</a></li>
+							<li class="divider"></li>
+							  <li><a tabindex="-1" href="commentary.php#intro">المقدمة</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter1">كتاب الطهارة</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter2">كتاب الصلاة</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter3">كتاب الصيام</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter4">كتاب الزكاة</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter5">كتاب الحج</a></li>
+							  <li><a tabindex="-1" href="commentary.php#chapter6">كتاب الزهد</a></li>	
+						</ul><!-- dropdown menu -->
+					</li>
+					<li><a href="audio.php">الصوتيات</a></li>
+					<li><a href="extras.php">المزيد</a></li>
+				</ul><!-- nav -->
+			</section><!-- navbar -->
+		</header>
+	</div><!-- column-->	
+</div> <!-- content -->

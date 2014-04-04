f2b41dc18f591e093abc4bd08bd0390145a5a406
Saleh <saleh.mayada@yahoo.com>
2014-04-04 08:35:25 +0300

    updated index page

--- index.php
+++ index.php
@@ -1,92 +1,85 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>التراث المالكي</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="_/css/bootstrap.css" rel="stylesheet">
-    <link href="_/css/mystyles.css" rel="stylesheet">
+<head>
+  <title>التراث المالكي</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="_/css/bootstrap.css" rel="stylesheet">
+  <link href="_/css/mystyles.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
 
-     <!--favicons-->
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="ico/favicon.png">
+      <!--favicons-->
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+      <link rel="shortcut icon" href="ico/favicon.png">
 
-  </head>
-  <body id="home">
-        <?php include "_/components/php/header.php"; ?>
-        
-         <div class="intro-header">
+    </head>
+    <body id="home">
 
+
+      <div class="jumbotron">
         <div class="container">
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="intro-message">
-                        <h1>التراث المالكي</h1>
-                        <h2>مشروع الكتاب الالكتروني</h2>
-                        <hr class="intro-divider">
-                        <a href="#feature" class="btn btn-default"><span class="glyphicon glyphicon-chevron-down"></span></a>
-                    </div>
-                </div>
-            </div>
-
+         <h1>التراث المالكي</h1>
+         <p>مشروع الكتاب الالكتروني</p>
+         <div class="btn-group left">
+          <button class="btn btn-default btn-lg dropdown-toggle " type="button" data-toggle="dropdown">الانتقال السريع<span class="caret"></span></button>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="book.php">قراءة المتن</a></li>
+            <li><a href="commentary.php">قراءة الشرح</a></li>
+            <li><a href="extras.php">الاستماع إلى الصوتيات</a></li>
+            <li class="divider"></li>
+            <li><a href="aboutproject.php">حول الكتاب</a></li>
+          </ul>
         </div>
-        <!-- /.container -->
+      </div>
 
     </div>
-    <!-- /.intro-header -->
 
-         <hr>
-      
 
-    <div class="container">
+    <div class="row" id="feature">
+      <div class="col col-xs-6 card" >
+        <h2>التحقيق العلمي</h2>
+        <p class="lead">من خلال ضبط النص الأصلي ضبطا دقيقا، وذلك بمقارنة النسخ المطبوعة من الكتاب بعضها ببعض، لمعرفة مواضع الخلل</p>
+      </div>
+      <div class="col-xs-6">
+        <img class="img-responsive card" src="images/poetryimg.jpg" alt="manuscript">
+      </div>
+    </div>
+    <hr/>
+    <div class="row">
+      <div class="col col-xs-6  col-xs-push-6 card">
+        <h2>استخدام الوسائل الحديثة</h2>
+        <p class="lead">وذلك من خلال الاعتماد على تقنيات الويب وأسلوب التصميم العصري للحصول على تجربة قراءة مريحة للقارئ سواء كان يتصفح الموقع على جهاز الحساوب المكتبي أو الحاسوب اللوحي أو حتى الهاتف النقال</p>
+      </div>
+      <div class="col-xs-6  col-xs-pull-6 card">
+        <img class="img-responsive" src="images/ebook.jpg" alt="technology">
+      </div>
+    </div>
+    <hr/>
+    <div class="row">
+      <div class="col-xs-6 card">
+        <h2>المحافظة على التراث</h2>
+        <p class="lead">الحرص على الاحتفاظ بالهوية الأصلية لكتب التراث وهيبتها وبالوقت نفسه إدخال لمسات فنية هدفها إظهار بعض المعلومات التي قد تشكل على المتعلم من خلال رسومات وخرائط ذهنية وغيرها من الوسائل التوضيحية</p>
+      </div>
+      <div class="col-xs-6 card">
+        <img class="img-responsive" src="images/scroll.jpg" alt="scrolls">
+      </div>
+    </div>
+    <hr>
 
-      <div class="row" id="feature">
-        <div class="col col-xs-6 card" >
-          <h2>التحقيق العلمي</h2>
-          <p class="lead">من خلال ضبط النص الأصلي ضبطا دقيقا، وذلك بمقارنة النسخ المطبوعة من الكتاب بعضها ببعض، لمعرفة مواضع الخلل</p>
-        </div>
-        <div class="col-xs-6">
-          <img class="img-responsive card" src="images/poetryimg.jpg" alt="manuscript">
-        </div>
-      </div>
-      <hr/>
-      <div class="row">
-        <div class="col col-xs-6  col-xs-push-6 card">
-          <h2>استخدام الوسائل الحديثة</h2>
-          <p class="lead">وذلك من خلال الاعتماد على تقنيات الويب وأسلوب التصميم العصري للحصول على تجربة قراءة مريحة للقارئ سواء كان يتصفح الموقع على جهاز الحساوب المكتبي أو الحاسوب اللوحي أو حتى الهاتف النقال</p>
-        </div>
-        <div class="col-xs-6  col-xs-pull-6 card">
-          <img class="img-responsive" src="images/ebook.jpg" alt="technology">
-        </div>
-      </div>
-      <hr/>
-      <div class="row">
-        <div class="col-xs-6 card">
-          <h2>المحافظة على التراث</h2>
-          <p class="lead">الحرص على الاحتفاظ بالهوية الأصلية لكتب التراث وهيبتها وبالوقت نفسه إدخال لمسات فنية هدفها إظهار بعض المعلومات التي قد تشكل على المتعلم من خلال رسومات وخرائط ذهنية وغيرها من الوسائل التوضيحية</p>
-        </div>
-        <div class="col-xs-6 card">
-          <img class="img-responsive" src="images/scroll.jpg" alt="scrolls">
-        </div>
-      </div>
-      <hr>
-      
-      <?php include "_/components/php/footer.php"; ?>
+    <?php include "_/components/php/footer.php"; ?>
     <!-- scripts  -->
     <script src="_/js/jquery.js"></script>
     <script src="_/js/bootstrap.js"></script>
-     <script src="_/js/myscripts.js"></script>
+    <script src="_/js/myscripts.js"></script>
 
   </body>
-</html>
+  </html>
